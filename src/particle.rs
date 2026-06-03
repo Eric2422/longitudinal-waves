f6738136fe89a35ed8e0eb46bd56ce47f3cb6e30
Eric Zhang <ericbkzhang@gmail.com>
2026-06-03 09:05:49 -0400

    Add constructor and `to_string()` methods for `Particle` struct

--- src/particle.rs
+++ src/particle.rs
@@ -1,6 +1,25 @@
 struct Particle {
-    mass: u32,
-    position: Vec<i32>,
-    velocity: Vec<i32>,
-    acceleration: Vec<i32>,
+    mass: f64,
+    position: Vec<f64>,
+    velocity: Vec<f64>,
+    acceleration: Vec<f64>,
+    linked_masses: Vec<Particle>,
+}
+
+impl ToString for Particle {
+    fn to_string(&self) -> String {
+        format!("{}, {:?}", self.mass, self.position)
+    }
+}
+
+impl Particle {
+    pub fn new() -> Particle {
+        Particle {
+            mass: 1.0,
+            position: vec![0.0, 0.0, 0.0],
+            velocity: vec![0.0, 0.0, 0.0],
+            acceleration: vec![0.0, 0.0, 0.0],
+            linked_masses: vec![],
+        }
+    }
 }
