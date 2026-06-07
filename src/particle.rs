7cdef30d8ce33f027a7b2ed3a3b46fe24c27df23
Eric Zhang <ericbkzhang@gmail.com>
2026-06-07 17:20:09 -0400

    Add an atmoic counter for the number of particles
    It is necessary to distinguish which particles are equal. Even if two
    particles have the same mass, position, velocity, acceleration, and
    linked masses (or lack thereof), they might not be guaranteed to be the same.

--- src/particle.rs
+++ src/particle.rs
@@ -1,8 +1,16 @@
 use std::collections::HashMap;
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    thread,
+};
+
+/// Counter for the [`id`](Particle::id) property of the [`Particle`] class.
+static PARTICLE_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 /// A single particle in a longitudinal wave, each connected to other particles
 /// by linear springs.
 struct Particle {
+    id: usize,
     mass: f64,
     position: Vec<f64>,
     velocity: Vec<f64>,
@@ -28,6 +36,7 @@ impl Particle {
     /// no linked masses.
     pub fn new() -> Particle {
         Particle {
+            id: PARTICLE_COUNTER.fetch_add(1, Ordering::SeqCst),
             mass: 1.0,
             position: vec![0.0, 0.0, 0.0],
             velocity: vec![0.0, 0.0, 0.0],
@@ -48,10 +57,40 @@ impl Particle {
 
 
 /// The builder for the [`Particle`] class.
-pub struct ParticleBuilder {}
+pub struct ParticleBuilder {
+    mass: f64,
+    position: Vec<f64>,
+    velocity: Vec<f64>,
+    acceleration: Vec<f64>,
+    linked_masses: HashMap<Particle, f64>,
+}
 
 impl ParticleBuilder {
     pub fn new() -> ParticleBuilder {
-        ParticleBuilder {}
+        ParticleBuilder {
+            mass: 1.0,
+            position: vec![0.0, 0.0, 0.0],
+            velocity: vec![0.0, 0.0, 0.0],
+            acceleration: vec![0.0, 0.0, 0.0],
+            linked_masses: HashMap::new(),
+        }
     }
+
+    pub fn set_mass(mut self, mass: f64) {
+        self.mass = mass;
+    }
+
+    pub fn set_position(mut self, x: f64, y: f64, z: f64) {
+        self.position = vec![x, y, z];
+    }
+
+    pub fn set_velocity(mut self, x: f64, y: f64, z: f64) {
+        self.velocity = vec![x, y, z];
+    }
+
+    pub fn set_acceleration(mut self, x: f64, y: f64, z: f64) {
+        self.acceleration = vec![x, y, z];
+    }
+
+    pub fn add_linked_mass(mut self, particle: Particle) {}
 }
