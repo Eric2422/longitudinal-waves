38f8283491d11e0ba7fd8843d32c3c2de0dd17f7
Eric Zhang <ericbkzhang@gmail.com>
2026-06-14 15:33:23 -0400

    Update reading dimensions to default to 1 if there are fewer than three dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::{env, fs};
 
-use crate::particle::{Particle, ParticleBuilder};
+use crate::particle::ParticleBuilder;
 
 mod particle;
 
@@ -18,28 +18,31 @@ pub struct InputJson {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // As a basic test of functionality, copy the input file into the output
-    // directory.
+    // Attempt to retreive the contents of the file.
     let file_contents = match fs::read_to_string(&args[1]) {
         Ok(file_contents) => file_contents,
         Err(_) => panic!("Error: File `{}` could not be read.", &args[1]),
     };
 
+    // Attempt to parse the file into usable data.
     let input_json: InputJson = match serde_json::from_str(&file_contents) {
         Ok(input_json) => input_json,
         Err(_) => panic!("Error: File `{}` is malformatted.", &args[1]),
     };
 
-    let mut dimensions = input_json.dimensions.clone();
-
-    if dimensions.len() < 2 {
-        println!("Warning: Y dimension not provided. Assuming 1.");
+    // If the number of dimensions provided is less than 3, pad the end with 1
+    // (i.e., there is 1 particle in that dimension).
+    let mut dimensions: [u32; 3] = [1, 1, 1];
+    if input_json.dimensions.len() < 3 {
+        println!("Warning: Less than 3 dimensions provided. Assuming 1 for missing dimensions.");
     }
-
-    if dimensions.len() < 3 {
-        println!("Warning: Z dimension not provided. Assuming 1.");
+    for i in 0..2 {
+        if input_json.dimensions.len() > i + 1 {
+            dimensions[i] = input_json.dimensions[i]
+        }
     }
 
     let spring_constant = input_json.spring_constant;
-    let particle = ParticleBuilder::new().set_mass(input_json.mass);
+    let builder = ParticleBuilder::new();
+    let particle = builder.set_mass(input_json.mass);
 }
